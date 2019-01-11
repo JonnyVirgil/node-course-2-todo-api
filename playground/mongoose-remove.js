525c88907f709012c329cc855ce76ec334a24fd6
Jon Kenyon <jon.kenyon@solopress.com>
2019-01-11 10:58:57 +0000

    Test the DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5c3866dc07fd320406d11ce4').then((todo) => {
+  console.log(todo);
+});
